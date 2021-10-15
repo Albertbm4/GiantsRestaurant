5c377408956664fbf9378e619c5d96bd9968d112
Borrellas, Albert <thealbertbm@gmail.com>
2021-10-15 17:32:42 +0200

    Final push

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,37 @@
 import './App.css';
-import RestaurantCard from './COMPONENTS/Card.js'
-import React from 'react';
-import Simple from './COMPONENTS/Carousel'
+
+import React, { useRef, useEffect, useState } from 'react';
+import Simple from './COMPONENTS/carousel'
 import "react-multi-carousel/lib/styles.css";
-//const mapboxgl = require('react-mapbox-gl');
 import mapboxgl from 'mapbox-gl'; 
 
-function App() {
-  return (
-    
+const BCN_LNG = 2.14;
+const BCN_LAT = 42.35;
 
+export default function App() {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(BCN_LNG);
+  const [lat, setLat] = useState(BCN_LAT);
+  const [zoom, setZoom] = useState(11);
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYWRyaWFsZW9uIiwiYSI6ImNrdXIzaDl3NzBnMWwyb2wwNXd3enN2aGgifQ.EO0AaoMf1sOhOxneYMBJBw';
+
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [lng,lat],
+      zoom: zoom
+    });
+  });
+
+  return (
       <body> 
         <Simple /> 
         <div ref={mapContainer} className="map-container" style={{'height': 800}}/>
-
       </body>
-    
   );
 }
-
-
